921e3987ed0c0eff7a41b06c625158718b1c6965
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-12-30 08:19:15 +0530

    socket url fixes.

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER_URL;
 
-const socket = io(`wss://${SOCKET_SERVER_URL}`, {
+const socket = io(SOCKET_SERVER_URL, {
   reconnectionDelayMax: 10000,
 });
 
